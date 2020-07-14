2f77452dc389efada5077d489e101fb5e1018d2a
Ruben d'Arco <cyclops@prof-x.net>
2020-07-14 19:10:57 +0100

    Multiple url's and all that.

--- FCG/getstatus.sh
+++ FCG/getstatus.sh
@@ -8,6 +8,15 @@ CARID[1]="c7b66665-de6c-41b4-b51f-6a41dbc10a88"
 CARID[2]="1f9c1ee6-b72d-4f90-b811-a931fb85674b"
 CARID[3]="9c6947d3-239d-473a-b5cf-0ac70b389d51"
 
+URL[0]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/car/${CARID[0]}/categories/purchase-documents"
+URL[1]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/car/${CARID[1]}/categories/purchase-documents"
+URL[2]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/car/${CARID[2]}/categories/purchase-documents"
+URL[3]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/car/${CARID[3]}/categories/purchase-documents"
+URL[4]="https://google.com"
+URL[5]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/document/${CARID[0]}"
+URL[6]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/document/${CARID[1]}"
+URL[7]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/document/${CARID[2]}"
+URL[8]="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/document/${CARID[3]}"
 
 if [ ! -e ${OUTPUT} ]
 then
@@ -16,13 +25,12 @@ fi
 
 while [ 1 ]
 do
-	for index in ${!CARID[*]}
+	for index in ${!URL[*]}
 	do
-		URL="https://europe-west1-fcg-dev-4.cloudfunctions.net/ar_k2-gateway/car/${CARID[$index]}/categories/purchase-documents"
 		TSTAMP=`date +%s`
-		TIMING=`curl -s -o /dev/null -w "@curl-format.txt" -H "accept: application/json" -X GET $URL`
-		echo ${TSTAMP}, ${CARID[$index]}, $TIMING
-		echo ${TSTAMP}, ${CARID[$index]}, $TIMING >> ${OUTPUT}
+		TIMING=`curl -s -o /dev/null -w "@curl-format.txt" -H "accept: application/json" -H 'Cache-Control: no-cache' -X GET ${URL[$index]}`
+		echo ${TSTAMP}, ${URL[$index]}, $TIMING
+		echo ${TSTAMP}, ${URL[$index]}, $TIMING >> ${OUTPUT}
 	done
 	sleep 3
 done
